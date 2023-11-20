cb00e2452b92ea79dd0c35cc19d253cfb5233314
Michael Neely <michael@michaelneely.net>
2023-11-20 13:35:04 -0500

    Added axios

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -59,14 +59,15 @@ function sendHttpRequest(method, url, data) {
 
 async function fetchPosts() {
     // try {
-        const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
-        const listOfPosts = responseData;
+        const response = await axios.get( 'https://jsonplaceholder.typicode.com/posts');
+        const listOfPosts = response.data;
         for (const post of listOfPosts) {
             const postEl = document.importNode(postTemplate.content, true);
             postEl.querySelector('h2').textContent = post.title.toUpperCase();
             postEl.querySelector('p').textContent = post.body;
             postEl.querySelector('li').id = post.id;
             listElement.append(postEl);
+
         }
     // } catch (error) {
     //     alert(error.message);
@@ -88,7 +89,8 @@ async function createPost(title, content) {
     fd.append('userId', userId);
 
 
-    sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', fd);
+    const response = await axios.post('https://jsonplaceholder.typicode.com/posts', fd);
+    console.log(response);
 }
 
 fetchButton.addEventListener('click', fetchPosts);
@@ -102,7 +104,7 @@ form.addEventListener('submit', event => {
 postList.addEventListener('click', event => {
     if (event.target.tagName === 'BUTTON') {
         const postId = event.target.closest('li').id;
-        sendHttpRequest('DELETE', `https://jsonplaceholder.typicode.com/posts/${postId}`);
+        axios.delete('DELETE', `https://jsonplaceholder.typicode.com/posts/${postId}`);
     }
 });
 
